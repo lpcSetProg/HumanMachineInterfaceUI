df0a3852f68bd0e82442a0abee82ca751631222a
Leveson Cocarell <Lcocarell2816@conestogac.on.ca>
2018-12-04 22:38:19 -0500

    design

--- HMI/HMI/MainWindow.xaml.cs
+++ HMI/HMI/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+* FILE : MainWindow.cs
+* PROJECT : PROG3165 - User Interface Design  - A04
+* PROGRAMMERS : Lev Cocarell and Bobby Vu
+* FIRST VERSION : 2018-04-12
+* DESCRIPTION :
+* This file contains the source code for the 
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +32,19 @@ namespace HMI
         public MainWindow()
         {
             InitializeComponent();
+            // Automatically resize height and width relative to content
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+            tabItem_Manual.IsEnabled = false;
+
+
+  
+        }
+
+     
+        private void button_stop_Click(object sender, RoutedEventArgs e)
+        {
+            tabItem_Manual.IsEnabled = true;
+            this.tabItem_Manual.Focus();
         }
     }
 }
